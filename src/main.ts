a399bd398bd2a971fdcbf28f194ab127dc1e1f27
Kateryna <vlkvskatia@gmail.com>
2026-04-09 18:03:32 +0300

    fix output file

--- src/main.ts
+++ src/main.ts
@@ -1,98 +1,80 @@
 import { createReadStream, createWriteStream } from 'node:fs';
-import { access } from 'node:fs/promises';
 import path from 'node:path';
 import { pipeline } from 'node:stream/promises';
 import { createGunzip, createGzip } from 'node:zlib';
+import crypto from 'crypto';
+import { Transform } from 'node:stream';
+
+type TransformFile = () => Transform;
+
+const createOutputFile = (file: string, extention?: string) => {
+	const { dir, name, ext } = path.parse(file);
+	const uniqueId = `${Date.now()}-${crypto.randomBytes(8).toString('hex')}`;
+	const newFileName = `${name}-${uniqueId}${extention}`;
+	return path.join(dir, newFileName);
+};
+
+const processFile = async (
+	path: string,
+	extension: string,
+	transformFile: TransformFile,
+	destination?: string,
+) => {
+	try {
+		const output = destination
+			? createOutputFile(destination, extension)
+			: createOutputFile(path, extension);
+
+		const transform = transformFile();
+		const source = createReadStream(path);
+		const destinationStream = createWriteStream(output);
+
+		await pipeline(source, transform, destinationStream);
+
+		return output;
+	} catch (error: any) {
+		console.error(
+			`Error while processing a file: ${error.message}`,
+		);
+	}
+};
 
 // 1
 async function compressFile(filePath: string) {
-    const outputFile = async () => {
-        let counter = 0;
-        while (true) {
-            let outputFilePath =
-                counter === 0
-                    ? `${filePath}.zip`
-                    : `${filePath}.${counter}.zip`;
-            try {
-                await access(outputFilePath);
-                counter++;
-            } catch (error: any) {
-                if (error.code === 'ENOENT') {
-                    return outputFilePath;
-                } else {
-                    console.log(`Error occured: ${error.message}`);
-                }
-            }
-        }
-    };
-
-    try {
-        const output = await outputFile();
-        const gzip = createGzip();
-        const source = createReadStream(filePath);
-        const destination = createWriteStream(output);
-
-        await pipeline(source, gzip, destination);
-        return output;
-    } catch (error: any) {
-        console.error(`Error while compressing a file: ${error.message}`);
-    }
+	return processFile(filePath, '.zip', createGzip);
 }
 
-(async () => console.log(await compressFile('src/index.txt')))();
+(async () => {
+	const result = await compressFile('src/index.txt');
+	console.log(result);
+})();
 
 // 2
 async function decompressFile(
-    compressedFilePath: string,
-    destinationFilePath: string,
+	compressedFilePath: string,
+	destinationFilePath: string,
 ) {
-    const outputFile = async () => {
-        let counter = 0;
-        const { dir, name, ext } = path.parse(destinationFilePath);
-        while (true) {
-            let outputFilePath =
-                counter === 0
-                    ? destinationFilePath
-                    : path.join(dir, `${name}.${counter}${ext}`);
-            try {
-                await access(outputFilePath);
-                counter++;
-            } catch (error: any) {
-                if (error.code === 'ENOENT') {
-                    return outputFilePath;
-                } else {
-                    console.log(`Error occured: ${error.message}`);
-                }
-            }
-        }
-    };
-
-    try {
-        const output = await outputFile();
-        const sourse = createReadStream(compressedFilePath);
-        const destination = createWriteStream(output);
-        const gunzip = createGunzip();
-        await pipeline(sourse, gunzip, destination);
-        return output;
-    } catch (error: any) {
-        console.log(`Error while decompressing a file: ${error.message}`);
-    }
+	return processFile(
+		compressedFilePath,
+		'.txt',
+		createGunzip,
+		destinationFilePath,
+	);
 }
 
 async function performCompressionAndDecompression() {
-    try {
-        const compressedResult = await compressFile('./src/index.txt');
-        if (!compressedResult) throw new Error('Compression failed');
-        console.log(compressedResult);
-        const decompressedResult = await decompressFile(
-            compressedResult,
-            './src/index_decompressed.txt',
-        );
-        console.log(decompressedResult);
-    } catch (error: any) {
-        console.error(
-            `Error while decompressing or compressing a file: ${error.message}`,
-        );
-    }
+	try {
+		const compressedResult = await compressFile('./src/index.txt');
+		if (!compressedResult) throw new Error('Compression failed');
+		const decompressedResult = await decompressFile(
+			compressedResult,
+			'./src/index_decompressed.txt',
+		);
+		console.log(decompressedResult);
+	} catch (error: any) {
+		console.error(
+			`Error while decompressing or compressing a file: ${error.message}`,
+		);
+	}
 }
 performCompressionAndDecompression();
